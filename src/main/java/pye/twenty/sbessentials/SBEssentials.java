35076fe83f54292fc01d3142b964764451c75782
20PI <20pimk@gmail.com>
2023-08-14 22:50:39 +0300

    made SBEssentials.addCommand static

--- src/main/java/pye/twenty/sbessentials/SBEssentials.java
+++ src/main/java/pye/twenty/sbessentials/SBEssentials.java
@@ -1,6 +1,7 @@
 package pye.twenty.sbessentials;
 
 import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
 import pye.twenty.sbessentials.command.Command;
 import pye.twenty.sbessentials.command.impl.TestCommand;
 
@@ -17,11 +18,11 @@ public enum SBEssentials {
     }
 
     private void registerCommands() {
-       addCommand("test", new TestCommand());
+       addCommand("test", new TestCommand(), plugin);
     }
 
-    private void addCommand(String command, Command executor) {
-        INSTANCE.getPlugin().getCommand(command).setExecutor(executor);
-        INSTANCE.getPlugin().getCommand(command).setTabCompleter(executor);
+    public static void addCommand(String command, Command executor, JavaPlugin plugin) {
+        plugin.getCommand(command).setExecutor(executor);
+        plugin.getCommand(command).setTabCompleter(executor);
     }
 }
